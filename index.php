ceb48f050bf802d256f89c2d5b3b0633507d24b1
tantyotyo <tantyotyo29@gmail.com>
2024-11-02 09:48:28 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perferendis eligendi quisquam eos iure. Est saepe veniam sunt unde laudantium minus, ipsum consequatur vitae mollitia obcaecati ipsa omnis numquam porro natus doloribus dolorem ratione atque, maxime ea consectetur. Laborum dolores possimus deserunt itaque dignissimos consequuntur libero non praesentium iure explicabo!</p>
 </body>
 
 </html>
